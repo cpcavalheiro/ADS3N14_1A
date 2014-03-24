42eb1114e62c1517f731a9c9ce82e66d8dc6f9bb
Cássio P cavalheiro <cassio.pcavalheiro@gmail.com>
2014-03-23 23:28:50 -0300

    Criados objetos para criar, ler e gravar o arquivo de dados na classe.

--- ListaTelefonica/src/lista/controller/ArquivoController.java
+++ ListaTelefonica/src/lista/controller/ArquivoController.java
@@ -1,7 +1,12 @@
 package lista.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.Writer;
 
 public class ArquivoController {
 
@@ -14,25 +19,38 @@ public class ArquivoController {
 	File file = new File("./dados/ListaTelefone.txt");
 
 	/**
-	 * Testa se arquivo de dados existe.
-	 * Se não existir chama o metodo geraArquivo()
-	 * Se existir chama o metodo acessaArquivo()
-	 * dados.
+	 * Testa se arquivo de dados existe. Se não existir chama o metodo
+	 * geraArquivo() Se existir chama o metodo acessaArquivo()
 	 * 
 	 * @author Cássio P Cavalheiro
 	 */
 	public void verificaArquivo() {
 		if (!file.exists()) {
 			geraArquivo();
-		}else{
+		} else {
 			acessaArquivo();
 		}
 	}
 
-	private void acessaArquivo() {
-		
+	/**
+	 * Faz a leitura do arquivo de dados.
+	 * 
+	 * @return ler
+	 */
+	private BufferedReader acessaArquivo() {
+		FileReader arq = null;
+		try {
+			arq = new FileReader(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		BufferedReader ler = new BufferedReader(arq);
+		return ler;
 	}
 
+	/**
+	 * Gera o arquivos de dados caso ele não exista.
+	 */
 	private void geraArquivo() {
 		try {
 			file.createNewFile();
@@ -41,5 +59,28 @@ public class ArquivoController {
 			e.printStackTrace();
 		}
 
+	}
+
+	/**
+	 * Grava informações no arquivo de dados.
+	 */
+	private void gravaArquivo(String linha) {
+		RandomAccessFile grava = null;
+		try{
+			 grava.writeBytes(linha);
+		}catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+          
+                if (grava != null)
+                    grava.close();
+                
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 	}
 }

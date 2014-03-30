dbedfd1469939826b0079c0c5475333a127d572e
Cassio P Cavalheiro <cassio.pcavalheiro@gmail.com>
2014-03-30 00:58:05 -0300

    #3 - Criado metodo main, criados objetos.

---
+++ BatalhaNaval/src/batalha/controller/BatalhaNaval.java
@@ -0,0 +1,23 @@
+package batalha.controller;
+
+import batalha.view.BatalhaView;
+
+public class BatalhaNaval {
+
+	public static void main(String[] args) {
+		BatalhaView view = new BatalhaView();
+		Controller controller = new Controller();
+
+		view.imprimeTabuleiro(controller.iniciaJogo());
+		controller.geraEmbarcacao();
+
+		/*
+		 * do {
+		 * 
+		 * } while (tabuleiro.getTiros() > 0);
+		 */
+		System.out.println("Fim do jogo!");
+
+	}
+
+}
